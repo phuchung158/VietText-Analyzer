a50f7db9ac100898187221e04089d773f8a95123
phuchung158 <144474225+phuchung158@users.noreply.github.com>
2026-06-03 00:59:54 +0700

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .preprocessing import normalize_text, tokenize_vietnamese, preprocess_pipeline, load_and_preprocess
+from .preprocessing import normalize_text, preprocess_pipeline, load_and_preprocess
 from .data_loader import DataLoader
 from .evaluator import Evaluator
 
